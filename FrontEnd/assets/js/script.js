af811350f369395568fc16d4514c70fac2cec764
Mael Donnart <maeldonnart@hotmail.fr>
2023-12-31 11:36:10 +0100

    Automatisation du code Js pour les filtres

---
+++ FrontEnd/assets/js/script.js
@@ -0,0 +1,100 @@
+//Recupération des éléments de la DOM
+
+const gallery = document.querySelector(".gallery")
+const filtersContainer = document.querySelector(".filter-container")
+let filters = []
+
+// Fonction d'affichage des travaux
+
+async function getWorks(){
+
+    let filteredWorks = []
+
+    try{
+        const apiResponse = await fetch("http://localhost:5678/api/works")
+
+        if (!apiResponse.ok) {
+
+            throw new Error(`Response has fail with the status ${apiResponse.status}`)
+
+        }else{
+
+            const works = await apiResponse.json()
+
+            //On crée un objet Set afin de compter le nombre d'élément avec un id unique présent dans l'objet works
+            const worksSet = new Set();
+
+            for (let i = 0; i < works.length; i++) {
+                worksSet.add(works[i].category.name)
+            }
+
+            //convertion en array de l'objet Set
+            const arrayWorksSet = Array.from(worksSet);
+
+            createFiltersButton(arrayWorksSet)
+
+            filters = document.querySelectorAll(".filter")
+            console.log(filters)
+
+            displayWorks(works)
+
+            for (let i = 0; i < filters.length; i++) {
+                filters[i].addEventListener("click", (event) => {
+                    for (let i = 0; i < filters.length; i++) {
+                        filters[i].classList.remove("selected-filter")      
+                    }
+                    event.target.classList.add("selected-filter")
+                    filteredWorks = works.filter((work) => work.category.id === parseInt(event.target.dataset.liId ))
+                    displayWorks(filteredWorks)
+                })
+            }
+
+            }
+
+            
+
+        }catch(error){
+        console.error('An error was encounter during the API execution : ',error)
+    }
+}
+
+// Fonction d'affichage des travaux
+
+function displayWorks(worksToDisplay){
+    gallery.innerHTML = ""
+    for (let i = 0; i < worksToDisplay.length; i++) {
+        const figureElement = document.createElement("figure")
+        const imageElement = document.createElement("img")
+        figureElement.setAttribute("data-id", worksToDisplay[i].category.id);
+        imageElement.src = worksToDisplay[i].imageUrl
+        imageElement.alt = worksToDisplay[i].title
+        const figcaptionElement = document.createElement("figcaption")
+        figcaptionElement.textContent = worksToDisplay[i].title
+        gallery.appendChild(figureElement)
+        figureElement.appendChild(imageElement)
+        figureElement.appendChild(figcaptionElement)
+    }
+}
+
+// Fonction de création des bouttons filtres
+
+function createFiltersButton(arrayWorksSet){
+    for (let i = 0; i < arrayWorksSet.length; i++) {
+        const liElement = document.createElement("li")
+        liElement.classList.add("filter")
+        liElement.setAttribute("data-li-id", i+1)
+        liElement.textContent = arrayWorksSet[i]
+        filtersContainer.appendChild(liElement)      
+    }
+}
+
+if(gallery) {
+    // on verifie que gallery existe bien dans la page avant de s'en servir au sein de getWorks
+    // Bonne pratique afin d'éviter de casser le site si l'élement de la DOM ou je dois afficher les éléments n'existe pas
+    getWorks();
+}
+
+
+
+
+
